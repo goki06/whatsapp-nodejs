6cc352051b0f3e0183a100be895b0dd3f8282fb6
goki06 <gokyuks@gmail.com>
2025-01-21 01:38:06 +0100

    Fix Puppeteer Chromium setup

--- install-chrome.cjs
+++ install-chrome.cjs
@@ -3,11 +3,15 @@ const puppeteer = require('puppeteer');
 (async () => {
     try {
         console.log('Überprüfe die Chromium-Installation mit Puppeteer...');
+
+        // Verwende Umgebungsvariable für den Chromium-Pfad
         const browser = await puppeteer.launch({
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/opt/render/project/.cache/puppeteer/chrome/linux-1108766/chrome-linux/chrome',
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
-        console.log('Chromium wurde erfolgreich installiert.');
+
+        console.log('Chromium wurde erfolgreich installiert unter:', browser.executablePath());
         await browser.close();
     } catch (error) {
         console.error('Fehler bei der Chromium-Installation mit Puppeteer:', error);
