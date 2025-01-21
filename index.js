a4546e05d115eac3dbe8613ca599436b9afb8f45
goki06 <gokyuks@gmail.com>
2025-01-21 11:18:35 +0100

    Force cache clear by updating version

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ const app = express();
         console.log('Überprüfe die Chromium-Installation mit Puppeteer...');
 
         const browser = await puppeteer.launch({
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
 
-        console.log('Chromium erfolgreich gestartet unter:', browser.executablePath());
+        console.log('Chromium erfolgreich gestartet unter:', process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath());
         await browser.close();
     } catch (error) {
         console.error('Fehler bei der Chromium-Installation mit Puppeteer:', error);
